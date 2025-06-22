a57948a1e3c3cc814a252bfda0a8c2dcc967af4a
David Rash <DavidR@test.com>
2025-06-22 17:37:59 +0300

    add animation 2

---
+++ src/companents/Footer/FooterContact.tsx
@@ -0,0 +1,32 @@
+import {valueContact} from "../../utils/constants.ts";
+import React, {useEffect, useRef} from "react";
+
+const FooterContact = () => {
+    const refContact = useRef<(HTMLLIElement | null)[]>([]);
+
+    useEffect(() => {
+        refContact.current.forEach((item) => {
+            if(item){
+                const icon = item.getAttribute("data-img-footer");
+                if (icon) {
+                    item.style.setProperty("--footer-icon", `url(${icon})`);
+                }
+            }
+        });
+    }, [])
+
+    return (
+            <ul className={"footer-list"}>
+                {
+                    valueContact.map((item, index) => (
+                        <li ref={(el) => {refContact.current[index] = el;}}
+                            className="footer-item" key={index} data-img-footer={item.icon}>
+                            <a className="footer-link" href={item.href} >{item.topic}</a>
+                        </li>
+                    ))
+                }
+            </ul>
+    );
+};
+
+export default FooterContact;
---
+++ src/utils/types.d.ts
@@ -0,0 +1,16 @@
+export type CardSkillsItem ={
+    title: string,
+    description: string,
+}
+
+export type PortfolioItem = {
+        title:string,
+        href: string,
+        description:string
+};
+
+export type ContactItem = {
+    href: string;
+    icon: string;
+    topic: string;
+};
